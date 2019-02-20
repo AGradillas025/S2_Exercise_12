237d8529f467458fa615fc02a95190250eb23b8a
Unknown <AGradillas025@west-mec.org>
2019-02-19 18:24:01 -0700

    First Commit (Displaying Calender)
    In this commit I was displaying the date inside the calender. Also, I learned more about arrays and how to use them.

--- lht_calendar.js
+++ lht_calendar.js
@@ -5,8 +5,8 @@
    Tutorial 10
    Tutorial Case
 
-   Author: 
-   Date:  
+   Author: Anthony S,A Gradillas
+   Date:  2.19.19
 
    Filename:   lht_calendar.js  
 
@@ -29,3 +29,22 @@
       Writes the daily rows in the calendar table, highlighting calDate
 	
 */
+
+/* Set the date displayed in the calender */
+var thisDay = new Date("August 24, 2018");
+
+// Write the calender to the element with the Id "calender"
+document.getElementById("calender").innerHTML = createCalender(thisDay);
+
+// Function to generate the calender table
+function createCalendar(calDate) {
+      var calenderHTML = "<table id='calender_table'>";
+      calenderHTML += "</table>";
+      return calenderHTML;
+}
+
+// Function to write the calender caption
+function calCaption(calDate) {
+      // monthName array contains the list of month names
+      var monthName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+}
